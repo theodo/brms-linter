efbd2d3332fb99f26ad3566acfcdb2f957d2da3a
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2020-04-09 12:10:09 +0200

    Add syntax error handling as first world errors

--- app.js
+++ app.js
@@ -2,23 +2,39 @@ const fs = require('fs');
 const antlr4 = require('antlr4');
 const BrmsLexer = require('./build/BrmsLexer').BrmsLexer;
 const BrmsParser = require('./build/BrmsParser').BrmsParser;
-const BrmsListener = require('./build/BrmsListener').BrmsListener;
+const BrmsSyntaxErrorListener = require('./BrmsSyntaxErrorListener')
+  .BrmsSyntaxErrorListener;
 const linterRules = require('./linterRules').linterRules;
 const logger = require('./logger').logger;
 
 const parseContent = (err, contents) => {
+  if (err) {
+    logger.logError(err);
+    process.exit(2);
+  }
+
   const chars = new antlr4.InputStream(contents);
   const lexer = new BrmsLexer(chars);
   const tokens = new antlr4.CommonTokenStream(lexer);
   const parser = new BrmsParser(tokens);
   parser.buildParseTrees = true;
+  parser.removeErrorListeners();
+  const brmsSyntaxErrorListener = new BrmsSyntaxErrorListener();
+  parser.addErrorListener(brmsSyntaxErrorListener);
   const { children: rules } = parser.rules();
+
+  const syntaxErrors = brmsSyntaxErrorListener.getErrors();
+  syntaxErrors.forEach(({ line, column, msg }) =>
+    logger.logError(`line ${line}, col ${column}: ${msg}`)
+  );
+
   const rulesValidationResult = Object.values(linterRules).map(
     (linterRuleValidator) => {
       return linterRuleValidator(rules);
     }
   );
-  if (!rulesValidationResult.every(({ result }) => result)) {
+  const hasLinterErrors = !rulesValidationResult.every(({ result }) => result);
+  if (hasLinterErrors) {
     rulesValidationResult
       .filter(({ result }) => !result)
       .forEach(({ linterRuleName, errors }) => {
@@ -26,6 +42,12 @@ const parseContent = (err, contents) => {
         logger.logInfo(errors);
       });
   }
+
+  if (syntaxErrors.length > 0 || hasLinterErrors) {
+    process.exit(1);
+  }
+
+  process.exit(0);
 };
 
 const filename = process.argv[2];
