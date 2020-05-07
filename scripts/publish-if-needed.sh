642d0f7467fd2baba07804db166bdff950228729
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2020-05-08 00:06:15 +0200

    :bug: Fix publishing status when not deploying on master

--- scripts/publish-if-needed.sh
+++ scripts/publish-if-needed.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# exit 0 if the current code version doesn't match the npm repository version
+# publish the package if the current code version doesn't match the npm repository version
 set -euo pipefail
 IFS=$'\n\t'
 
@@ -7,8 +7,8 @@ NPM_CURRENT_VERSION="$(npm view brmslint version)"
 CODE_VERSION="$(jq '.version' package.json --raw-output)"
 
 if [[ "${NPM_CURRENT_VERSION}" != "${CODE_VERSION}" ]]; then
+  npm publish --access public
   exit 0
 fi
-echo >&2 "Should not publish package as current \
+echo "Should not publish package as current \
 version '${CODE_VERSION}' match currently published npm version '${NPM_CURRENT_VERSION}'"
-exit 1
